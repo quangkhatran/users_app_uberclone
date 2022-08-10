6d40e4a6a48c118a12041fc3ad3411cd22a7eb9f
quangkhatran <quangkhatran@gmail.com>
2022-08-10 18:21:48 +0700

    create Splash Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+import './splashScreen/splash_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -12,7 +14,7 @@ void main() async {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        // home: MySplashScreen(),
+        home: MySplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
     ),
---
+++ lib/splashScreen/splash_screen.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+import '../global/global.dart';
+import '../mainScreens/main_screen.dart';
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  State<MySplashScreen> createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+  startTimer() {
+    Timer(const Duration(seconds: 3), () async {
+      if (await fAuth.currentUser != null) {
+        currentFirebaseUser = fAuth.currentUser;
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (c) => MainScreen(),
+          ),
+        );
+      } else {
+        // Navigator.push(
+        //   context,
+        //   MaterialPageRoute(
+        //     builder: (c) => LoginScreen(),
+        //   ),
+        // );
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.black,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('images/logo.png'),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                'Uber & inDriver Clone App',
+                style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
